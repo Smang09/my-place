d2327ea44508eaef2da29ab54fa0f37d7f9d2c3b
DESKTOP-R5647O8\user <kwak9747@naver.com>
2025-11-12 12:18:20 +0900

    Updates

---
+++ assets/images/index.ts
@@ -0,0 +1,27 @@
+import image1 from './01.jpg';
+import image2 from './02.jpg';
+import image3 from './03.jpg';
+import image4 from './04.jpg';
+import image5 from './05.jpg';
+import image6 from './06.jpg';
+import image7 from './07.jpg';
+import image8 from './08.jpg';
+import image9 from './09.jpg';
+import image10 from './10.jpg';
+import image11 from './11.jpg';
+import image12 from './12.jpg';
+
+export default [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+];
